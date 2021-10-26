a27ba40d80ade86f9f442e9b0566021354ae609f
AKoz4Code <64234460+LogicAtom@users.noreply.github.com>
2021-10-26 04:02:59 -0400

    update env_sample.py
    delete all of it

--- env_sample.py
+++ env_sample.py
@@ -1,7 +1,1 @@
-import os
 
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "%*klPqRBoV,HZP;19S?7dgf/]es_Lq")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://Rosie:Ceetoun2021@cluster0.60h5u.mongodb.net/task_manager?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "task_manager")
